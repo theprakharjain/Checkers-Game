05823de825e1e920cf2661fe21724592f89283f5
Prakhar Jain <theprakharjain@gmail.com>
2020-09-10 22:42:46 +0530

    Checkers game - made and displayed Board

---
+++ checkers/__init__.py
@@ -0,0 +1,3 @@
+# This "__init__.py" file in the checkers folder tells the program that
+# checkers folder is a package
+# and thats why we are able to import the constant variables from "constants.py" to "main.py" via import method
---
+++ checkers/constants.py
@@ -0,0 +1,14 @@
+import pygame
+
+# Size of the board and squares on it
+WIDTH, HEIGHT = 800, 800
+ROWS, COLS = 8, 8
+SQUARE_SIZE = WIDTH//COLS
+
+
+# Colors
+RED = (255, 0, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+BLUE = (0, 0, 255)
+GREY = (128, 128, 128)
